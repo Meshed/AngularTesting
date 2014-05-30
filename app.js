06354788f94462cbe45a66c4dee29082591a7e69
unknown <MarkBrown@markbrown7.priv.life-south.com>
2014-05-30 12:24:39 -0400

    Initial add

---
+++ app.js
@@ -0,0 +1,21 @@
+/**
+ * Created by MarkBrown on 5/30/2014.
+ */
+var myModule = angular.module('Angello', []);
+
+myModule.controller('MainCtrl', function($scope) {
+    $scope.currentStory;
+
+    $scope.setCurrentStory = function(story){
+        $scope.currentStory = story;
+    };
+
+    $scope.stories = [
+        {title:'Story 00', description:'Description pending.'},
+        {title:'Story 01', description:'Description pending.'},
+        {title:'Story 02', description:'Description pending.'},
+        {title:'Story 03', description:'Description pending.'},
+        {title:'Story 04', description:'Description pending.'},
+        {title:'Story 05', description:'Description pending.'}
+    ];
+});
