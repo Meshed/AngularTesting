117fefb35c96f916af4c9221118951e14cd210ef
Mark Brown <marksbrown@gmail.com>
2014-06-02 17:10:34 -0400

    Added type and status selectors

--- app.js
+++ app.js
@@ -6,16 +6,116 @@ var myModule = angular.module('Angello', []);
 myModule.controller('MainCtrl', function($scope) {
     $scope.currentStory;
 
-    $scope.setCurrentStory = function(story){
-        $scope.currentStory = story;
+    var buildIndex = function(source, property){
+        var tempArray = [];
+
+        for(var i = 0, len = source.length; i < len; ++i){
+            tempArray[source[i][property]] = source[i];
+        }
+
+        return tempArray;
     };
 
+    $scope.setCurrentStatus = function(status){
+        if(typeof $scope.currentStory !== 'undefined'){
+            $scope.currentStory.status = status.name;
+        }
+    };
+
+    $scope.setCurrentType = function(type){
+        if(typeof $scope.currentType !== 'undefined'){
+            $scope.currentStory.type = type.name;
+        }
+    }
+
+    $scope.typesIndex = buildIndex($scope.types, 'name');
+    $scope.statusesIndex = buildIndex($scope.statuses, 'name');
+
+    $scope.createStory = function() {
+        $scope.stories.push({
+            title:'New Story',
+            description:'Description pending.'
+        });
+    };
+
+    $scope.setCurrentStory = function(story){
+        $scope.currentStory = story;
+
+        $scope.currentStatus = $scope.statusesIndex[story.status];
+        $scope.currentType = $scope.typesIndex[story.type];
+    };
+
+    $scope.statuses = [
+        {name:'Back Log'},
+        {name:'To Do'},
+        {name:'In Progress'},
+        {name:'Code Review'},
+        {name:'QA Review'},
+        {name:'Verified'},
+        {name:'Done'}
+    ];
+
+    $scope.types = [
+        {name:'Feature'},
+        {name:'Enhancement'},
+        {name:'Bug'},
+        {name:'Spike'}
+    ];
+
     $scope.stories = [
-        {title:'Story 00', description:'Description pending.'},
-        {title:'Story 01', description:'Description pending.'},
-        {title:'Story 02', description:'Description pending.'},
-        {title:'Story 03', description:'Description pending.'},
-        {title:'Story 04', description:'Description pending.'},
-        {title:'Story 05', description:'Description pending.'}
+        {
+            title:'Story 00',
+            description:'Description pending.',
+            criteria:'Criteria pending',
+            status:'To Do',
+            type:'Feature',
+            reporter:'Lukas Ruebbelke',
+            assignee:'Brian Ford'
+        },
+        {
+            title:'Story 01',
+            description:'Description pending.',
+            criteria:'Criteria pending',
+            status:'To Do',
+            type:'Feature',
+            reporter:'Lukas Ruebbelke',
+            assignee:'Brian Ford'
+        },
+        {
+            title:'Story 02',
+            description:'Description pending.',
+            criteria:'Criteria pending',
+            status:'To Do',
+            type:'Feature',
+            reporter:'Lukas Ruebbelke',
+            assignee:'Brian Ford'
+        },
+        {
+            title:'Story 03',
+            description:'Description pending.',
+            criteria:'Criteria pending',
+            status:'To Do',
+            type:'Feature',
+            reporter:'Lukas Ruebbelke',
+            assignee:'Brian Ford'
+        },
+        {
+            title:'Story 04',
+            description:'Description pending.',
+            criteria:'Criteria pending',
+            status:'To Do',
+            type:'Feature',
+            reporter:'Lukas Ruebbelke',
+            assignee:'Brian Ford'
+        },
+        {
+            title:'Story 05',
+            description:'Description pending.',
+            criteria:'Criteria pending',
+            status:'To Do',
+            type:'Feature',
+            reporter:'Lukas Ruebbelke',
+            assignee:'Brian Ford'
+        }
     ];
 });
